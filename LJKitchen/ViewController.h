c7a58e899c15bdb1c35f2d1cac639a21e88afd11
liji <154906186@qq.com>
2016-06-07 14:25:08 +0800

    Initial Commit

---
+++ LJKitchen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJKitchen
+//
+//  Created by  a on 16/6/7.
+//  Copyright © 2016年 ycdsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
